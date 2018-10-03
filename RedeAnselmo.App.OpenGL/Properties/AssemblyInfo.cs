285e23e7231232811b1e0cab0348f46751f32982
Yan Paulo <yanpaulo@hotmail.com>
2018-10-03 01:39:57 -0300

    Renomear assembly.

--- RedeAnselmo.App.OpenGL/Properties/AssemblyInfo.cs
+++ RedeAnselmo.App.OpenGL/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("RedeRebelde.App.OpenGL")]
+[assembly: AssemblyTitle("RedeAnselmo.App.OpenGL")]
 [assembly: AssemblyProduct("RedeRebelde.App.OpenGL")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyDescription("")]
